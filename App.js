82f299cd597cbfb52d53b52574e34ee53fb0825d
Mustafa <mgurbuz9696@gmail.com>
2021-01-11 21:09:32 +0300

    some changes made in App.js

--- App.js
+++ App.js
@@ -9,16 +9,7 @@ import Router from './src/router';
 
 export default class App extends Component {
   componentWillMount() {
-    const config = {
-      apiKey: "AIzaSyDlM2-wWpshJaTxXVd4Y9QwipS4QsAmjvY",
-      authDomain: "first-react-project-9fc83.firebaseapp.com",
-      databaseURL: "https://first-react-project-9fc83-default-rtdb.firebaseio.com",
-      projectId: "first-react-project-9fc83",
-      storageBucket: "first-react-project-9fc83.appspot.com",
-      messagingSenderId: "715195073317",
-      appId: "1:715195073317:web:a974b97cbfa6f2375710cc",
-      measurementId: "G-HT997D3Y3P"
-    };
+
     if (!firebase.apps.length) {
       firebase.initializeApp(config);
     }
